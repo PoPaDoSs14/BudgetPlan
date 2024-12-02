6231b1918cc0a00cd4f3c4dfd3885bbd495e9a22
PoPaDoSs14 <slasevvladimir7@gmail.com>
2024-12-02 20:31:41 +0300

    add viewBinding in RegistrationActivity

--- app/src/main/java/com/example/budgetplan/presentation/RegistrationActivity.kt
+++ app/src/main/java/com/example/budgetplan/presentation/RegistrationActivity.kt
@@ -3,11 +3,16 @@ package com.example.budgetplan.presentation
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.budgetplan.R
+import com.example.budgetplan.databinding.ActivityMainBinding
 
 class RegistrationActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
     }
 }
