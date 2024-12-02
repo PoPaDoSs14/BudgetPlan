3e126bca65eb71b73ae332d8e9afad97c2007eb3
PoPaDoSs14 <slasevvladimir7@gmail.com>
2024-12-02 20:20:51 +0300

    add RegistrationViewModel

---
+++ app/src/main/java/com/example/budgetplan/presentation/RegistrationViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.budgetplan.presentation
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class RegistrationViewModel(application: Application): AndroidViewModel(application) {
+
+
+
+}
