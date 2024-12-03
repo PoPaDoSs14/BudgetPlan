a1cc6ea70c1248f133591815fda384de26103010
PoPaDoSs14 <slasevvladimir7@gmail.com>
2024-12-03 18:23:09 +0300

    add save user after click button

--- app/src/main/java/com/example/budgetplan/domain/User.kt
+++ app/src/main/java/com/example/budgetplan/domain/User.kt
@@ -1,10 +1,11 @@
 package com.example.budgetplan.domain
 
 data class User(
-    val id: Int,
+    val id: Int = 0,
     val name: String,
     val money: Int,
     val monthProfit: Int,
     val monthLosses: Int,
-    val lastTask: Task
+    val lastTask: Task?
 )
+
--- app/src/main/java/com/example/budgetplan/presentation/RegistrationActivity.kt
+++ app/src/main/java/com/example/budgetplan/presentation/RegistrationActivity.kt
@@ -4,15 +4,25 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.budgetplan.R
 import com.example.budgetplan.databinding.ActivityMainBinding
+import com.example.budgetplan.domain.User
 
 class RegistrationActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: RegistrationViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        viewModel = RegistrationViewModel(application)
+
+
+        binding.registerButton.setOnClickListener {
+            val name = binding.username.text.toString()
+            val user = User(0, name, 0, 0, 0, null)
+            viewModel.addUser(user)
+        }
 
     }
 }
