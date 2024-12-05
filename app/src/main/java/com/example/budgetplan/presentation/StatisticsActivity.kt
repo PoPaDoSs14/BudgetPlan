cebc3472a79656bd6e002f250aa163fbfb45c3be
PoPaDoSs14 <slasevvladimir7@gmail.com>
2024-12-05 18:38:49 +0300

    add binding in StatisticsActivity

--- app/src/main/java/com/example/budgetplan/presentation/StatisticsActivity.kt
+++ app/src/main/java/com/example/budgetplan/presentation/StatisticsActivity.kt
@@ -6,17 +6,16 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.budgetplan.R
+import com.example.budgetplan.databinding.ActivityStatisticsBinding
 
 
 class StatisticsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityStatisticsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_statistics)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityStatisticsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
