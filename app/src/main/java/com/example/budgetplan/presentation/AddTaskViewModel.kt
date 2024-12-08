357ee60d955daa9df1bf1432d8deb0926003cafe
PoPaDoSs14 <slasevvladimir7@gmail.com>
2024-12-08 19:52:27 +0300

    add AddTaskViewModel

---
+++ app/src/main/java/com/example/budgetplan/presentation/AddTaskViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.budgetplan.presentation
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class AddTaskViewModel(application: Application): AndroidViewModel(application) {
+
+}
