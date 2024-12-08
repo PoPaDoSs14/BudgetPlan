598aa97f0a377114545467ac201e9a0f81b413fb
PoPaDoSs14 <slasevvladimir7@gmail.com>
2024-12-08 19:54:18 +0300

    add binding in AddTaskActivity

--- app/src/main/java/com/example/budgetplan/presentation/AddTaskActivity.kt
+++ app/src/main/java/com/example/budgetplan/presentation/AddTaskActivity.kt
@@ -6,10 +6,17 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.budgetplan.R
+import com.example.budgetplan.databinding.ActivityAddTaskBinding
+import com.example.budgetplan.databinding.ActivityStatisticsBinding
 
 class AddTaskActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAddTaskBinding
+    private lateinit var viewModel: AddTaskViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_task)
+        binding = ActivityAddTaskBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
