6e3bfeaa1bbb18f834c976cfde727264501346bb
PoPaDoSs14 <slasevvladimir7@gmail.com>
2024-12-19 20:24:53 +0300

    update RegistrationActivity

--- app/src/main/java/com/example/budgetplan/presentation/RegistrationActivity.kt
+++ app/src/main/java/com/example/budgetplan/presentation/RegistrationActivity.kt
@@ -5,6 +5,7 @@ import android.graphics.Color
 import android.os.Bundle
 import android.provider.CalendarContract.Colors
 import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.example.budgetplan.R
@@ -14,14 +15,13 @@ import com.example.budgetplan.domain.User
 class RegistrationActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private lateinit var viewModel: RegistrationViewModel
+    private val viewModel: RegistrationViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        viewModel = ViewModelProvider(this).get(RegistrationViewModel::class.java)
         viewModel.firstLogin(this)
 
         binding.registerButton.setOnClickListener {
@@ -30,24 +30,36 @@ class RegistrationActivity : AppCompatActivity() {
     }
 
     private fun registerUser() {
-        val name = binding.username.text.toString().trim()
+        val username = binding.username.text.toString().trim()
 
-        if (name.isNotEmpty()) {
-            val user = User(0, name, 0, 0, 0, null)
-            try {
-                viewModel.addUser(user)
-                navigateToStatistics()
-            } catch (e: Exception) {
-                Toast.makeText(baseContext, "Ошибка при добавлении пользователя: ${e.message}", Toast.LENGTH_SHORT).show()
+        when {
+            username.isEmpty() -> {
+                showToast("Введите имя")
             }
-        } else {
-            Toast.makeText(baseContext, "Введите имя", Toast.LENGTH_SHORT).show()
+            else -> {
+                val user = User(0, username, 0, 0, 0, null)
+                addUser(user)
+            }
+        }
+    }
+
+    private fun addUser(user: User) {
+        try {
+            viewModel.addUser(user)
+            navigateToStatistics()
+        } catch (e: Exception) {
+            showToast("Ошибка при добавлении пользователя: ${e.message}")
         }
     }
 
     private fun navigateToStatistics() {
-        val intent = Intent(this, StatisticsActivity::class.java)
-        startActivity(intent)
+        Intent(this, StatisticsActivity::class.java).also {
+            startActivity(it)
+        }
         finish()
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(baseContext, message, Toast.LENGTH_SHORT).show()
     }
 }
